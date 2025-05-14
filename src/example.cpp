2fa190f256a6d73cf3b6fecba7dfd80c9ec3237a
tony <anthony.peter.thompson@pm.me>
2025-05-14 10:38:19 +0100

    made project template

---
+++ src/example.cpp
@@ -0,0 +1,5 @@
+#include "kalman_filter/example.h"
+
+int add(int a, int b) {
+    return a + b;
+}
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "kalman_filter/example.h"
+
+int main() {
+    std::cout << "Result: " << add(2, 3) << std::endl;
+    return 0;
+}
