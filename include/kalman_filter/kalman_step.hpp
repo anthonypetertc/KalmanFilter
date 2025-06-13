81a3e4cf367245aed1186fd9802e11f4474239e5
tony <anthony.peter.thompson@pm.me>
2025-06-13 12:23:32 +0100

    updated to new structure

--- include/kalman_filter/kalman_step.hpp
+++ include/kalman_filter/kalman_step.hpp
@@ -2,32 +2,43 @@
 #ifndef INCLUDE_KALMAN_FILTER_KALMAN_STEP_HPP_  // Include guard
 #define INCLUDE_KALMAN_FILTER_KALMAN_STEP_HPP_
 
-#include <Eigen/Dense>  // dynamic size matrices and good performance
+#include <Eigen/Dense> 
 #include <iostream>
 
 class KalmanStep {
   // TODO(Tony): Need to implement usage of parameters.
-  // int dx;       // size of the vector representing the dependent
-  // int dy;       // size of the vector representing the
-  // independent variables.
-  // int dparams;  // size of vector of parameters.
+  // int dx;       // size of the vector representing the state.
+  // int dy;       // size of the vector representing the measurement.
+  // int du;  // size of the control vector.
 
-  Eigen::VectorXd x;  // dependent variables.
-  Eigen::VectorXd y;  // independent variables (measured).
+
+  Eigen::VectorXd y;  // measurement outcome.
+  Eigen::VectorXd u;  // control vector.
+  Eigen::VectorXd A; // Linearized dynamics (dependence on previous state).
+  Eigen::VectorXd B; // Linearized dynamics (dependence on control vector).
   Eigen::MatrixXd R;  // Measurement noise, should be of size (dy x dy)
-  Eigen::MatrixXd Q;  // System noise, should be of size (dparams x dparams)
+  Eigen::MatrixXd Q;  // System noise, should be of size (dx x dx)
+  Eigen::MatrixXd H; // Relates the measurement to the state (dy x dx)
 
  public:
   // constructor
-  KalmanStep(const Eigen::VectorXd& x, const Eigen::VectorXd& y,
-             const Eigen::MatrixXd& R, const Eigen::MatrixXd& Q, int dx, int dy,
-             int dparams);
+  KalmanStep(const Eigen::VectorXd& y, 
+             const Eigen::VectorXd& u,
+             const Eigen::MatrixXd& A, 
+             const Eigen::MatrixXd& B,
+             const Eigen::MatrixXd& R,
+             const Eigen::MatrixXd& Q,
+             const Eigen::MatrixXd& H
+            );
 
   // get attributes
-  Eigen::VectorXd getx() const;
   Eigen::VectorXd gety() const;
+  Eigen::VectorXd getu() const;
+  Eigen::VectorXd getA() const;
+  Eigen::VectorXd getB() const;
   Eigen::MatrixXd getR() const;
   Eigen::MatrixXd getQ() const;
+  Eigen::MatrixXd getH() const;
   double getdx() const;
   double getdy() const;
   double getdparams() const;
