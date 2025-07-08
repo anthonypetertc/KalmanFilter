b55ad781fe16323c63f48520df98a4610d3b67ff
anthonypetertc <anthonypetertc@hotmail.com>
2025-07-08 15:50:27 +0100

    Added a further unit test.

--- tests/test_kalman_filter.cpp
+++ tests/test_kalman_filter.cpp
@@ -2,6 +2,7 @@
 #include <Eigen/Dense>
 #include <boost/test/included/unit_test.hpp>
 #include <stdexcept>
+#include <vector>
 
 #include "kalman_filter/kalman_filter.hpp"
 #include "kalman_filter/kalman_step.hpp"
@@ -93,4 +94,49 @@ BOOST_AUTO_TEST_CASE(TestUpdate) {
   BOOST_CHECK_CLOSE(kalmanfilter.getKg()(0, 0), 0.5, 1e-6);
   BOOST_CHECK_CLOSE(kalmanfilter.getP()(0, 0), 0.5, 1e-6);
   BOOST_CHECK_SMALL(kalmanfilter.getEstimate()(0), 1e-6);
+}
+
+BOOST_AUTO_TEST_CASE(TestKGDecreasesWithNoise) {
+  Eigen::VectorXd init_estimate(1);
+  init_estimate << 0;
+
+  Eigen::MatrixXd init_P(1, 1);
+  init_P << 1;
+
+  int du = 1;
+  int dy = 1;
+  KalmanFilter Kf(init_estimate, init_P, du, dy);
+
+  Eigen::VectorXd y(1);
+  y << -0.1;
+
+  Eigen::VectorXd u(1);
+  u << 0;
+
+  Eigen::MatrixXd A(1, 1);
+  A << 1;
+
+  Eigen::MatrixXd B(1, 1);
+  B << 0;
+
+  Eigen::MatrixXd Q(1, 1);
+  Q << 0;
+
+  Eigen::MatrixXd H(1, 1);
+  H << 1;
+
+  std::vector<double> v1{0.001f, 1000.0f};
+  std::vector<double> kgs;
+  kgs.reserve(2);
+
+  for (std::size_t k = 0; k < 2; ++k) {
+    Eigen::MatrixXd R(1, 1);
+    R << v1[k];
+
+    KalmanStep next_step(y, u, A, B, R, Q, H);
+    Kf.update(next_step);
+    kgs.emplace_back(Kf.getKg()(0, 0));
+  }
+
+  BOOST_TEST(kgs[0] > kgs[1]);
 }
